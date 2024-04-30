3db1c0856558ba2f23b2a7042ba534d8f56477b5
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-04-30 06:36:49 +0300

    ADD Payment Class to Data-Access and Business Layers

---
+++ StudyCenterBusiness/clsPayment.cs
@@ -0,0 +1,113 @@
+﻿using StudyCenterDataAccess;
+using System;
+using System.Data;
+
+namespace StudyCenterBusiness
+{
+    public class clsPayment
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int? PaymentID { get; set; }
+        public int? StudentGroupID { get; set; }
+        public int? SubjectGradeLevelID { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int? CreatedByUserID { get; set; }
+
+        public clsStudentGroup StudentGroupInfo { get; private set; }
+        public clsSubjectGradeLevel SubjectGradeLevelInfo { get; private set; }
+        public clsUser CreatedByUserInfo { get; private set; }
+
+        public clsPayment()
+        {
+            PaymentID = null;
+            StudentGroupID = null;
+            SubjectGradeLevelID = null;
+            PaymentAmount = 0M;
+            PaymentDate = DateTime.Now;
+            CreatedByUserID = null;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsPayment(int? paymentID, int? studentGroupID, int? subjectGradeLevelID,
+            decimal paymentAmount, DateTime paymentDate, int? createdByUserID)
+        {
+            PaymentID = paymentID;
+            StudentGroupID = studentGroupID;
+            SubjectGradeLevelID = subjectGradeLevelID;
+            PaymentAmount = paymentAmount;
+            PaymentDate = paymentDate;
+            CreatedByUserID = createdByUserID;
+
+            StudentGroupInfo = clsStudentGroup.Find(studentGroupID);
+            SubjectGradeLevelInfo = clsSubjectGradeLevel.Find(subjectGradeLevelID);
+            CreatedByUserInfo = clsUser.Find(createdByUserID);
+
+            Mode = enMode.Update;
+        }
+
+        private bool _Add()
+        {
+            PaymentID = clsPaymentData.Add(StudentGroupID, SubjectGradeLevelID,
+                PaymentAmount, CreatedByUserID);
+
+            return (PaymentID.HasValue);
+        }
+
+        private bool _Update()
+        {
+            return clsPaymentData.Update(PaymentID, StudentGroupID, SubjectGradeLevelID,
+                PaymentAmount, CreatedByUserID);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_Add())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _Update();
+            }
+
+            return false;
+        }
+
+        public static clsPayment Find(int? paymentID)
+        {
+            int? studentGroupID = null;
+            int? subjectGradeLevelID = null;
+            decimal paymentAmount = 0M;
+            DateTime paymentDate = DateTime.Now;
+            int? createdByUserID = null;
+
+            bool isFound = clsPaymentData.GetInfoByID(paymentID, ref studentGroupID,
+                ref subjectGradeLevelID, ref paymentAmount, ref paymentDate, ref createdByUserID);
+
+            return (isFound) ? (new clsPayment(paymentID, studentGroupID,
+                                subjectGradeLevelID, paymentAmount, paymentDate, createdByUserID))
+                             : null;
+        }
+
+        public static bool Delete(int? paymentID)
+        => clsPaymentData.Delete(paymentID);
+
+        public static bool Exists(int? paymentID)
+        => clsPaymentData.Exists(paymentID);
+
+        public static DataTable All()
+        => clsPaymentData.All();
+    }
+}
---
+++ StudyCenterDataAccess/clsPaymentData.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudyCenterDataAccess
+{
+    public class clsPaymentData
+    {
+        public static bool GetInfoByID(int? paymentID, ref int? studentGroupID,
+            ref int? subjectGradeLevelID, ref decimal paymentAmount,
+            ref DateTime paymentDate, ref int? createdByUserID)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_GetPaymentInfoByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@PaymentID", (object)paymentID ?? DBNull.Value);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // The record was found
+                                isFound = true;
+
+                                studentGroupID = (reader["StudentGroupID"] != DBNull.Value) ? (int?)reader["StudentGroupID"] : null;
+                                subjectGradeLevelID = (reader["SubjectGradeLevelID"] != DBNull.Value) ? (int?)reader["SubjectGradeLevelID"] : null;
+                                paymentAmount = (decimal)reader["PaymentAmount"];
+                                paymentDate = (DateTime)reader["PaymentDate"];
+                                createdByUserID = (reader["CreatedByUserID"] != DBNull.Value) ? (int?)reader["CreatedByUserID"] : null;
+                            }
+                            else
+                            {
+                                // The record was not found
+                                isFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+                clsDataAccessHelper.HandleException(ex);
+            }
+
+            return isFound;
+        }
+
+        public static int? Add(int? studentGroupID, int? subjectGradeLevelID,
+            decimal paymentAmount, int? createdByUserID)
+        {
+            // This function will return the new person id if succeeded and null if not
+            int? paymentID = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_AddNewPayment", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@StudentGroupID", (object)studentGroupID ?? DBNull.Value);
+                        command.Parameters.AddWithValue("@SubjectGradeLevelID", (object)subjectGradeLevelID ?? DBNull.Value);
+                        command.Parameters.AddWithValue("@PaymentAmount", paymentAmount);
+                        command.Parameters.AddWithValue("@CreatedByUserID", (object)createdByUserID ?? DBNull.Value);
+
+                        SqlParameter outputIdParam = new SqlParameter("@NewPaymentID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        paymentID = (int?)outputIdParam.Value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsDataAccessHelper.HandleException(ex);
+            }
+
+            return paymentID;
+        }
+
+        public static bool Update(int? paymentID, int? studentGroupID,
+            int? subjectGradeLevelID, decimal paymentAmount, int? createdByUserID)
+        {
+            int rowAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_UpdatePayment", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@PaymentID", (object)paymentID ?? DBNull.Value);
+                        command.Parameters.AddWithValue("@StudentGroupID", studentGroupID);
+                        command.Parameters.AddWithValue("@SubjectGradeLevelID", subjectGradeLevelID);
+                        command.Parameters.AddWithValue("@PaymentAmount", paymentAmount);
+                        command.Parameters.AddWithValue("@CreatedByUserID", createdByUserID);
+
+                        rowAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                clsDataAccessHelper.HandleException(ex);
+            }
+
+            return (rowAffected > 0);
+        }
+
+        public static bool Delete(int? paymentID)
+        => clsDataAccessHelper.Delete("SP_DeletePayment", "PaymentID", paymentID);
+
+        public static bool Exists(int? paymentID)
+        => clsDataAccessHelper.Exists("SP_DoesPaymentExist", "PaymentID", paymentID);
+
+        public static DataTable All()
+        => clsDataAccessHelper.All("SP_GetAllPayments");
+    }
+}
